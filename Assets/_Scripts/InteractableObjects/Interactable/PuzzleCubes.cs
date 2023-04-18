277aed4b9d425306736bf2fc0e87058a5766328f
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-04-19 00:51:55 +0300

    Puzzle Cubes Movement Finished

--- Assets/_Scripts/InteractableObjects/Interactable/PuzzleCubes.cs
+++ Assets/_Scripts/InteractableObjects/Interactable/PuzzleCubes.cs
@@ -2,32 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using Game.Interafaces;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Game.InteractableObjects
 {
     public class PuzzleCubes : MonoBehaviour, IInteractable
     {
+        private readonly Vector3[] _calculateVectors = new[] { Vector3.back, Vector3.forward, Vector3.up, Vector3.down };
+        private bool _canMove = true;
+
         public void Execute()
         {
-            CheckNeighbour(Vector3.back);
+            if (!_canMove) return;
+
+            for (int i = 0; i < _calculateVectors.Length; i++)
+            {
+                if (!CheckNeighbour(_calculateVectors[i]))
+                {
+                    break;
+                }
+            }
         }
 
         private bool CheckNeighbour(Vector3 position)
         {
-            RaycastHit hit;
-
-            if (Physics.Raycast(transform.position, position, out hit, 1))
+            if (Physics.Raycast(transform.position, position, out RaycastHit hit, 1))
             {
                 print(hit.collider.gameObject.name);
                 return true;
             }
-            else
-            {
-                
-            }
 
+            _canMove = false;
+            transform.DOMove(position, 0.5f)
+                .SetRelative()
+                .OnComplete(() =>
+                {
+                    // Puzzle Complete or Not 
+                    _canMove = true;
+                });
             return false;
         }
-        
     }
 }
