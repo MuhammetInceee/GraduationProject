b5e4b7f149f2b902c9bfc804a4276c37efce986f
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-04-19 00:02:12 +0300

    Refactoring

--- Assets/_Scripts/MainPlayer/Conditions.cs
+++ Assets/_Scripts/MainPlayer/Conditions.cs
@@ -7,24 +7,9 @@ namespace MainPlayer.Conditions
     public class Conditions : MonoBehaviour
     {
         internal bool CanCheck = true;
-        private StateMachine _stateMachine;
 
         public bool isGrounded;
         public bool isInInteraction;
-
-
-
-
-        private void Awake()
-        {
-            _stateMachine = GetComponent<StateMachine>();
-        }
-
-        void Update()
-        {
-            print(_stateMachine._currentState._originSO.name);
-        }
-        
 
         public IEnumerator Jump()
         {
